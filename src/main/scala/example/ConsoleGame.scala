284b3fef144fce5fc2ef37268ce5c423f2345e30
Michael Donaghy <md401@srcf.ucam.org>
2018-09-11 16:44:17 +0100

    Use FS2 to interleave effects in a controlled way

--- src/main/scala/example/ConsoleGame.scala
+++ src/main/scala/example/ConsoleGame.scala
@@ -1,55 +1,51 @@
 package example
 
+import cats.data.State
+import cats.effect.IO
+import fs2.Stream
 import org.fusesource.jansi.{Ansi, AnsiConsole}
 import jline.console.{ConsoleReader, KeyMap, Operation}
 
-import scala.concurrent.{ExecutionContext, Future, blocking}
-import java.util.concurrent.atomic.AtomicBoolean
-import java.util.concurrent.ArrayBlockingQueue
-
-import cats.data.State
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.duration._
 
 object ConsoleGame extends App {
+  implicit val timer = IO.timer
+
   val reader = new ConsoleReader()
-  val isGameOn = new AtomicBoolean(true)
-  val keyPressses = new ArrayBlockingQueue[Either[Operation, String]](128)
+
   case class GameState(pos: (Int, Int))
-  var gameState: GameState = GameState(pos = (6, 7))
 
   AnsiConsole.out.println(Draw.eraseScreen.run(Ansi.ansi()).value._1)
 
-  import ExecutionContext.Implicits._
+  val km = KeyMap.keyMaps().get("vi-insert")
+  val inputHandling = Stream.repeatEval(IO {
+    val c = reader.readBinding(km)
+    if (c == Operation.SELF_INSERT) Right(reader.getLastBinding)
+    else Left(c match { case op: Operation => op })
+  }).map {
+    case Right("q") | Left(Operation.VI_EOF_MAYBE) =>
+      None
+    case Left(op) => Some(op)
+  }.unNoneTerminate
 
-  // inside a background thread
-  val inputHandling = Future {
-    val km = KeyMap.keyMaps().get("vi-insert")
-    while (isGameOn.get) {
-      blocking {
-        val c = reader.readBinding(km)
-        val k: Either[Operation, String] =
-          if (c == Operation.SELF_INSERT) Right(reader.getLastBinding)
-          else Left(c match { case op: Operation => op })
-        keyPressses.add(k)
+  val ticks = Stream.unfoldEval[IO, Int, Int](0) { tick => IO.sleep(100 milliseconds).map(_ => Some((tick + 1, tick + 1))) }
+
+  val inputAndTicks = inputHandling map Left.apply mergeHaltL (ticks map Right.apply)
+
+  inputAndTicks.evalMapAccumulate(GameState(pos = (6, 7))) {
+    case (gameState, Left(op)) =>
+      val nextState = handleKeypress(op, gameState)
+      IO.pure((nextState, nextState))
+    case (gameState, Right(tick)) =>
+      IO {
+        if (tick % 10 == 0) {
+          info("something ".concat(tick.toString))
+        }
+        drawGame(gameState)
+        (gameState, gameState)
       }
-    }
-  }
-
-  var tick: Int = 0
-
-  // inside the main thread
-  while (isGameOn.get) {
-    while (!keyPressses.isEmpty) {
-      Option(keyPressses.poll) foreach { k =>
-        gameState = handleKeypress(k, gameState)
-      }
-    }
-    tick += 1
-    if (tick % 10 == 0) {
-      info("something ".concat(tick.toString))
-    }
-    drawGame(gameState)
-    Thread.sleep(100)
-  }
+  }.compile.drain.unsafeRunSync()
 
   def info(msg: String): Unit = {
     AnsiConsole.out.println(Ansi.ansi()
@@ -59,25 +55,22 @@ object ConsoleGame extends App {
       .a(msg))
   }
 
-  def handleKeypress(k: Either[Operation, String], g: GameState): GameState =
+  def handleKeypress(k: Operation, g: GameState): GameState =
     k match {
-      case Right("q") | Left(Operation.VI_EOF_MAYBE) =>
-        isGameOn.set(false)
-        g
       // Left arrow
-      case Left(Operation.BACKWARD_CHAR) =>
-        val pos0 = gameState.pos
+      case Operation.BACKWARD_CHAR =>
+        val pos0 = g.pos
         g.copy(pos = (pos0._1 - 1, pos0._2))
       // Right arrow
-      case Left(Operation.FORWARD_CHAR) =>
+      case Operation.FORWARD_CHAR =>
         val pos0 = g.pos
         g.copy(pos = (pos0._1 + 1, pos0._2))
       // Down arrow
-      case Left(Operation.NEXT_HISTORY) =>
+      case Operation.NEXT_HISTORY =>
         val pos0 = g.pos
         g.copy(pos = (pos0._1, pos0._2 + 1))
       // Up arrow
-      case Left(Operation.PREVIOUS_HISTORY) =>
+      case Operation.PREVIOUS_HISTORY =>
         g
       case _ =>
         // println(k)
