aee9c8a8a1a1003ebfa34ee7be051906633b44f9
Michael Donaghy <md401@srcf.ucam.org>
2018-09-11 15:36:46 +0100

    Only depend on fs2, not -io

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -4,5 +4,5 @@ object Dependencies {
   val scalaTest = "org.scalatest" %% "scalatest" % "3.0.5"
   val jansi = "org.fusesource.jansi" % "jansi" % "1.17.1"
   val jline3 = "jline" % "jline" % "2.14.6"
-  val fs2 = "co.fs2" %% "fs2-io" % "0.10.4"
+  val fs2 = "co.fs2" %% "fs2-core" % "0.10.4"
 }
